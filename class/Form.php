3e5a36c0b23db819690c46142a717470d013b1c8
stievenk <stieven.kalengkian@gmail.com>
2025-02-13 10:05:21 +0800

    Form Update

--- class/Form.php
+++ class/Form.php
@@ -839,7 +839,7 @@ class Form {
 	   * @param	mixed	$tipe	Tipe option: year|month
 	   * @param string	$default	Default value for selected option
 	  */
-	  function option_list($tipe,$default='') {
+	  function option_list($tipe,$default='',$yearstart='') {
 		$tipe = strtolower($tipe);
 		$year = date("Y");
 		switch($tipe) {
@@ -851,7 +851,8 @@ class Form {
 			break;
 			case 'year' :
 			case 'tahun' :
-				for($i = 2020; $i<=$year+2; $i++) {
+				$yearstart = $yearstart ? $yearstart : 2020;
+				for($i = $yearstart ; $i<=$year+2; $i++) {
 					echo '<option value="'.$i.'" '. ($i == $default ? 'selected' : '') .'>'. $i .'</option>';
 				}
 			break;
@@ -953,6 +954,24 @@ class Form {
 		} else {
 			return '';
 		}
+	}
+
+	function funText($text) {
+		$text = preg_replace("#\*(.+?)\*#s","<b>\\1</b>",$text);
+		$text = preg_replace("#\~(.+?)\~#s","<i>\\1</i>",$text);
+		$text = preg_replace("#\[blue\](.+?)\[\/blue\]#s","<span class=\"text-primary\">\\1</span>",$text);
+		$text = preg_replace("#\[red\](.+?)\[\/red\]#s","<span class=\"text-danger\">\\1</span>",$text);
+		$text = preg_replace("#\[img src=\"(.+?)\"\]#s","<img src=\"\\1\" class=\"img-fluid\">",$text);
+		$text = preg_replace("#\[url=(.+?)\](.+?)\[url\]#s","<span goto-url=\"\\1\" class=\"bg-white ps-1 pe-1 text-primary\">\\2 <i class=\"fa fa-link\"></i></span>",$text);
+		$text = preg_replace("#\[page=(.+?)\](.+?)\[page]\]#s","<span goto-page=\"\\1\" class=\"bg-white ps-1 pe-1 text-primary\">\\2 <i class=\"fa fa-link\"></i></span>",$text);
+		$text = preg_replace("#```(.+?)```#s","<pre class=\"bg-light p-1 border rounded\">\\1</pre>",$text);
+
+		$text = preg_replace("#\(\^\.\^\)|\(\^\^\)#si",'<img src="'.$this->serverURL().'images/emot/pappukangi.gif" height="25">',$text);
+		$text = preg_replace("#\[emot=mdc\]#si",'<img src="'.$this->serverURL().'images/logo_mdc.png" onerror="this.src=\''.$this->serverURL().'images/logo.png\'" style="max-height:50px">',$text);
+		$text = preg_replace("#\[emot=(.+?)\]#si",'<img src="'.$this->serverURL().'images/emot/\\1.gif" onerror="this.src=\''.$this->serverURL().'images/logo.png\'" style="max-height:50px">',$text);
+		$text = preg_replace("#\[sticker=(.+?)\]#si",'<img src="'.$this->serverURL().'images/stciker/\\1.png" onerror="this.src=\''.$this->serverURL().'images/logo.png\'" style="max-height:65px">',$text);
+		$text = preg_replace("#\@(.+?) #s","<span class=\"text-primary\" goto-page=\"?call=module&mod=profile&f=view&uname=\\1\">@\\1</span> ",$text);
+		return $text;
 	}
 
     function __destruct() {
