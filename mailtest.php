f756a6d6f20230c52640fc33cfa160e9c16d0b6b
stievenk <stieven.kalengkian@gmail.com>
2025-03-16 10:36:15 +0800

    Mail test

---
+++ mailtest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Koyabu\Webapi;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
+require_once  $MASTER_PATH . 'autoload.php';
+
+$config['smtp_host'] = 'dimanado.biz.id';
+$config['smtp_port'] = '465';
+$config['smtp_user'] = 'info@dimanado.biz.id';
+$config['smtp_pass'] = 'angiejowendankenzhi8007';
+$config['SITE_NAME'] = 'Test Email';
+
+$API = new KoyabuAPI($config);
+$option = [
+   'to' => 'stieven.kalengkian@gmail.com',
+   'from' => ['email' => $config['smtp_user'] , 'name' => $config['SITE_NAME']],
+   'subject' => 'Email percobaan',
+   'body' => 'Terima Kasih'
+];
+if (!$API->sendMail($option)) {
+   echo $API->error;
+}
+?>
