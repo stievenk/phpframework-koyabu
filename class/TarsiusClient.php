da9260ccd70ef2fb6c0ac3c2ee2fc56b70bf7c87
stievenk <stieven.kalengkian@gmail.com>
2025-05-09 08:37:51 +0800

    TarsiusClient

--- class/TarsiusClient.php
+++ class/TarsiusClient.php
@@ -11,12 +11,13 @@ use GuzzleHttp\Psr7\Response;
 // use GuzzleHttp\Exception\ServerException;
 // use GuzzleHttp\Exception\ConnectException;
 
-class TarsiusClient extends Form {
+class TarsiusClient {
 
    public $server_url = 'http://localhost/projects/Android-Apps/Android_12-13/CS_CONTROLMANAGER/web/api/';
+   private $URL = 'https://app.photoboothmanado.com/';  
 
-   function __construct($config) {
-      parent::__construct($config);
+   function __construct(public $config) {
+      // parent::__construct($config);
    }
    function setServerURL($url) {
       $this->server_url = $url;
@@ -30,6 +31,41 @@ class TarsiusClient extends Form {
       $data = $res->getBody();
       return json_decode($data,true);
       // print_r($data);
+   }
+
+   public function getStatus() {
+      if (!$this->config['application']) {
+         echo 'Invalid Application Config!';
+         exit;
+      }
+      $params = ['command' => 'getStatus', 'data' => $this->config['application']];
+      $res = $this->run($this->URL,$params);
+      // echo $res;
+      $s = json_decode($res,true);
+
+
+      if ($s['status'] == 'ON') {
+          return true;
+      } else {
+         $this->errorInfo($s);
+         return false;
+      }
+   }
+
+   public function requestKey() {
+      $params = ['command' => 'requestKey', 'data' => $this->config['application']];
+      return json_decode($this->run($this->URL.'',$params),true);
+   }
+
+   public function run($URL,$data) {
+      $client = new \GuzzleHttp\Client();
+      $response = $client->request('POST', $URL,[ \GuzzleHttp\RequestOptions::JSON => $data , 'timeout' => 30 ]);
+      return $response->getBody();
+   }
+
+   public function errorInfo($s) {
+     echo '<div style="padding:15px"><h1>Server Error!</h1><p>Mohon maaf layanan aplikasi tidak bisa digunakan silahkan hubungi Administrator anda ('.$s['response'].')</p></div>';
+     exit;
    }
 
 }
