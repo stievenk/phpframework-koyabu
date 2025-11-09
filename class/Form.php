5749535610a69ed04b5a52a0a4fed16b4d7ea407
Stieven Kalengkian <stieven.kalengkian@gmail.com>
2025-11-09 15:15:33 +0800

    New save Method

--- class/Form.php
+++ class/Form.php
@@ -6,7 +6,7 @@ use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\Output\QROutputInterface;
 /** 
  * Koyabu Framework
- * version: 8.2.2
+ * version: 8.2.3
  * last update: 4 Oktober 2025
  * min-require: PHP 8.1 
  * MariaDB: 10+ (recommended) or MySQL : 8+
